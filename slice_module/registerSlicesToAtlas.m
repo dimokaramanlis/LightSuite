0d0ecced6fe5899a97225fa3180c53047d766495
Dimokratis Karamanlis <dimokaramanlis@gmail.com>
2025-06-19 10:41:29 +0200

    better handling when no cp present

--- slice_module/registerSlicesToAtlas.m
+++ slice_module/registerSlicesToAtlas.m
@@ -4,25 +4,31 @@ function regparams = registerSlicesToAtlas(opts)
 
 regparams = struct();
 %==========================================================================
-% read reg volume and control points
+% read reg volume and options
 volpath  = dir(fullfile(opts.procpath,'*20um.tif'));
-cppath   = dir(fullfile(opts.procpath,'*tform.mat'));
 optspath = dir(fullfile(opts.procpath,'*regopts.mat'));
 
-% load volume and control points
 dp       = fullfile(volpath.folder, volpath.name);
-dpcp     = fullfile(cppath.folder,   cppath.name);
 dpopts   = fullfile(optspath.folder,   optspath.name);
 
-% volume   = readDownStack(fullfile(opts.procpath, 'volume_for_inspection.tiff'), 1);
-
 volume   = readDownStack(dp);
-cpdata   = load(dpcp);
 regopts  = load(dpopts);
 
 % we permute the volume to match atlas
 volume  = permute(volume, regopts.howtoperm);
 Nslices = size(volume, 1);
+%==========================================================================
+% load control points if available
+cppath   = dir(fullfile(opts.procpath,'*tform.mat'));
+if ~isempty(cppath)
+    dpcp              = fullfile(cppath.folder, cppath.name);
+    cpdata            = load(dpcp);
+    histology_cpoints = cpdata.histology_control_points;
+    atlas_cpoints     = cpdata.atlas_control_points;
+else
+    histology_cpoints = repmat({zeros(0,2)}, [Nslices 1]);
+    atlas_cpoints     = repmat({zeros(0,2)}, [Nslices 1]);
+end
 %==========================================================================
 downfac_reg = regopts.allenres/regopts.registres;
 nfac        = regopts.extentfactor;
@@ -52,53 +58,23 @@ ysamplevals      = linspace(Rvolume.YWorldLimits(1), Rvolume.YWorldLimits(2), Ns
 ysamplevals      = ysamplevals(1:end-1) + median(diff(ysamplevals))/2;
 [~, atlasinds]   = min(pdist2(ysamplevals',yatlasvals'), [],2);
 
-hascp            = ~cellfun(@isempty, cpdata.atlas_control_points);
-useratlasinds    = cellfun(@(x) x(1,1), cpdata.atlas_control_points(hascp));
+%=========================================================================
+% deal with missing control points
+hascp            = ~cellfun(@isempty,   atlas_cpoints);
+useratlasinds    = cellfun(@(x) x(1,1), atlas_cpoints(hascp));
 if nnz(hascp) > 3
     % refine remaining
     pfit      = polyfit(atlasinds(hascp), useratlasinds, 1);
     atlasinds = round(polyval(pfit, atlasinds));
 end
 atlasinds(hascp) = useratlasinds;
-
-%%
-% clf;
-% scatter3(cptshistology(:, 1), cptshistology(:, 2), cptshistology(:, 3))
-% hold on;
-% scatter3(cpatlasori(:, 1), cpatlasori(:, 2), cpatlasori(:, 3))
-% 
-% tformout = fitRigidTrans3D(cpatlasori, cptshistology);
-% % tformout = fitAffineTrans3D(cpatlasori, cptshistology);
-% 
-% newatlas = tformout.transformPointsForward(cpatlasori);
-% clf;
-% scatter3(cptshistology(:, 1), cptshistology(:, 2), cptshistology(:, 3))
-% hold on;
-% scatter3(newatlas(:, 1), newatlas(:, 2), newatlas(:, 3))
-% 
-% raatlas  = imref3d(size(tv),  0.5, 0.5, 0.5);
-% rasample = imref3d(size(volume), 1, regopts.pxsizes(1), 1);
-% tvtest   = imwarp(tv, raatlas, tformout,'linear', 'OutputView',rasample);
-% tvoritest = imwarp(tv, raatlas, opts.tform_rigid_AllenToSample_20um,'linear', 'OutputView',rasample);
-%%
-% clf;
-% for ishow = 1:Nslices
-% % ishow = 45;
-%     subplot(1,3,1)
-%     imagesc(squeeze(volume(ishow, :,:))); axis equal; axis tight;
-%     subplot(1,3,2)
-%     imagesc(squeeze(tvoritest(ishow,:,:))); axis equal; axis tight;
-%     subplot(1,3,3)
-%     imagesc(squeeze(tvnew(atlasinds(ishow),:,:))); axis equal; axis tight;
-%     pause;
-% end
-%%
 %=========================================================================
 % for every slice, we fit an affine transform from atlas to the slice
-% if no control points are available, we use point clouds
+% if no control points are available, we use only image info
 fprintf('Using control points and elastix atlas fitting...\n'); 
-wholetic = tic;
-msg = repmat('=', [1 100]);
+
+wholetic = tic; msg = repmat('=', [1 100]);
+
 ratlas      = imref2d(size(tvnew,  [2 3]));
 rahist      = imref2d(size(volume, [2 3]));
 slicetforms = affinetform2d;
@@ -118,22 +94,24 @@ for islice = 1:Nslices
     atlasim   = squeeze(tvnew(atlasinds(islice),:,:));
     annotim   = squeeze(avnew(atlasinds(islice),:,:));
     
-    fixedpts  = cpdata.histology_control_points{islice}(:, [3 2]);
+    fixedpts  = histology_cpoints{islice}(:, [3 2]);
     fixedpts  = reshape(fixedpts, [], 2);
-    movingpts = cpdata.atlas_control_points{islice}(:, [3 2]);
+    movingpts = atlas_cpoints{islice}(:, [3 2]);
     movingpts = reshape(movingpts, [], 2);
     Nmov      = size(movingpts, 1);
     %------------------------------------------------------------------
     % affine estimation
 
-    if isempty(movingpts) || (size(movingpts, 1) < 5)
-        % TO DO!!!!!
+    if isempty(movingpts) || (Nmov < 5)
         tformcurr = fitPointCloudsAffine(atlasim, histim, opts.registres);
+        tstruse   = sprintf('Not enough control points, using only image...\n\n');
     else
         tformcurr = fitgeotform2d(movingpts, fixedpts, 'affine');
+        tstruse   = sprintf('%d control points found, thanks for the effort!\n\n', Nmov);
     end
 
     slicetforms(islice, 1) = tformcurr;
+    fprintf(tstruse)
     %------------------------------------------------------------------
     % bspline estimation
     movptsaffine = tformcurr.transformPointsForward(movingpts);
@@ -172,26 +150,7 @@ for islice = 1:Nslices
 end
 % we save the affine registrations to the output structure
 fprintf('DONE with all slices! Took %d min\n', ceil(toc(wholetic)/60));
-%%
 %=========================================================================
-
-
-
-
-% 
-% % we have to take the transformed atlas and zero it out
-% % we fill in the atlas the slices we chose to register
-% % we undo the rigid transform to go back to the original atlas space
-% 
-% rcurr = imref3d([1 size(testim)], 1,  7.5,1);
-% [avnew, rnew]  = imwarp(reshape(testim, [1, size(testim)]),...
-%     rcurr, opts.tform_rigid_AllenToSample_20um.invert, 'nearest');
-
-%%
-%=========================================================================
-%%
-
-%==========================================================================
 % data saving
 
 regparams.atlasres         = regopts.allenres;
