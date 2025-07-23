189dab2ae7907b129ef529da5cd67257804c3a39
Dimokratis Karamanlis <dimokaramanlis@gmail.com>
2025-07-23 17:22:18 +0200

    fixed bug in sanitizing cells

--- atlas_counting/sanitizeCellCoords.m
+++ atlas_counting/sanitizeCellCoords.m
@@ -5,7 +5,7 @@ function finalpts = sanitizeCellCoords(cellcoords, annvol)
 finalpts = [round(cellcoords(:, 1:3)) cellcoords(:, 4:5)]; % rounding to transfer correspondance to pixels
 %--------------------------------------------------------------------------
 % remove cells outside volume
-irem0 = any(finalpts<1, 2);
+irem0 = any(finalpts(:,1:3)<1, 2);
 iremx = finalpts(:,1) > size(annvol, 2);
 iremy = finalpts(:,2) > size(annvol, 1);
 iremz = finalpts(:,3) > size(annvol, 3);
@@ -14,7 +14,7 @@ finalpts(irem, :) = [];
 %--------------------------------------------------------------------------
 % remove cells outside brain regions (root)
 indcells = sub2ind(size(annvol), finalpts(:,2), finalpts(:,1), finalpts(:,3));
-irem     = annvol(indcells) == 1;
+irem     = annvol(indcells) == 0;
 finalpts(irem, :) = [];
 %--------------------------------------------------------------------------
 end
